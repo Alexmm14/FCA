4b618ef7e84ceaef2cd2ad5c661e57bfb7eae7e3
marvaz10 <76980605+marvaz10@users.noreply.github.com>
2021-03-24 16:44:06 -0600

    Tarea 1

--- 2_semestre/Tarea1.cpp
+++ 2_semestre/Tarea1.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
 int main(){
-    printf("Nombre: Cristian Martín Vázquez González.\n18 años\nTeléfono:5547353499.\nCorreo electrónico: marvaz10@protonmail.com");
+    printf("Nombre: Cristian Martín Vázquez González\nEdad:18años\nTeléfono:5547353499.\nCorreo electrónico: marvaz10@protonmail.com");
     return 0;
 }
