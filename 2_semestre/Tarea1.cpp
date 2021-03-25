96c93b9f405baa474257a5103177e0ee4306b093
marvaz10 <76980605+marvaz10@users.noreply.github.com>
2021-03-24 22:57:19 -0600

    Traspaso de ejecutables a gitignore

--- 2_semestre/Tarea1.cpp
+++ 2_semestre/Tarea1.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<locale.h>
 int main(){
-    printf("Nombre: Cristian Martín Vázquez González\nEdad:18años\nTeléfono:5547353499.\nCorreo electrónico: marvaz10@protonmail.com");
+    setlocale(LC_ALL,"spanish");
+    printf("Nombre: Cristian Martín Vázquez González\nEdad:18 años\nTeléfono:5547353499.\nCorreo electrónico: marvaz10@protonmail.com");
     return 0;
 }
