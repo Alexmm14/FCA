b1e5c415a3a69f261672420557962335ee3808cd
marvaz10 <76980605+marvaz10@users.noreply.github.com>
2021-04-10 14:35:53 -0500

    Programa de número romanos de 5 cifras

--- 2_semestre/Númerosromanos2.cpp
+++ 2_semestre/Númerosromanos2.cpp
@@ -4,23 +4,37 @@ using namespace std;
 int main (){
     setlocale(LC_ALL,"spanish");
     int x,uni,dec,cen,mil,dimil;
+    char eleccion;
+    do{
+    system("cls");
     cout<<"Este programa convierte un número arábigo de 5 dígitos en un número romano.";
-    cout<<"\nDigita un número de 5 dígitos: ";cin>>x;
+    cout<<"\nDigita un número de máximo 5 dígitos: ";cin>>x;
     uni=x%10;x/=10;
     dec=x%10;x/=10;
     cen=x%10;x/=10;
     mil=x%10;x/=10;
-    dimil=x%10;x/=10;
+    dimil=x%10;x/=10;cout<<"\n";
+    switch (dimil){
+    case 1: printf("_\nX");break;
+    case 2: printf("_\nXX");break;
+    case 3: printf("___\nXXX");break;
+    case 4: printf("__\nXL");break;
+    case 5: printf("_\nL");break;
+    case 6: printf("__\nLX");break;
+    case 7: printf("___\nLXX");break;
+    case 8: printf("____\nLXXX");break;
+    case 9: printf("__\nXC");break;
+    }
     switch (mil){
     case 1: printf("M");break;
     case 2: printf("MM");break;
     case 3: printf("MMM");break;
-    case 4: printf("");break;
-    case 5: printf("");break;
-    case 6: printf("");break;
-    case 7: printf("");break;
-    case 8: printf("");break;
-    case 9: printf("");break;
+    case 4: printf("__\nIV");break;
+    case 5: printf("_\nV");break;
+    case 6: printf("__\nVM");break;
+    case 7: printf("___\nVMM");break;
+    case 8: printf("____\nVMMMM");break;
+    case 9: printf("__\nIX");break;
     }
     switch (cen){
     case 1: printf("C");break;
@@ -55,5 +69,8 @@ int main (){
     case 8: printf("VIII");break;
     case 9: printf("X");break;
     }
+    cout<<"\n\n¿Quieres traducir otro número?\nDigita 's' o 'n': ";cin>>eleccion;
+    }while(eleccion=='s');
+    system("cls");cout<<"FIN DEL PROGRAMA\n";system("pause");
     return 0;
 }
