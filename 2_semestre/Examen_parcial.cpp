4af030bf81ece5fecceb9597965c7ceef3a126e1
marvaz10 <marvaz10@protonmail.com>
2021-04-29 09:55:50 -0500

    Penúltima versión

--- 2_semestre/Examen_parcial.cpp
+++ 2_semestre/Examen_parcial.cpp
@@ -23,25 +23,16 @@ void consultas();
 void despliegues();
 void ordenes();
 
-struct info_direccion{//Estructura de datos de dirección
-	char calle[30];
-	char colonia[30];
-	char municipio[30];
-	char numex[30];
-	char numin[30];
-	char cp[30];
+struct direccion{//Estructura de datos de dirección
+	string calle,colonia,municipio,cp;
+    int numex,numin;
 };
 
-
-struct empleado{//Estructura de datos generales
-	char nombre[30];
-    char apellidopat[30];
-    char apellidomat[30];
-    char email[30];
-    char edad[30];
-	struct info_direccion dir_empleado;
-    char telefono[30];
-}empleados[100];
+struct persona{//Estructura de datos generales
+	string nombre,apellidopat,apellidomat,email,edad,telefono;
+	struct direccion dir;
+};
+persona e[100];
 
 int main(){//Función principal
     setlocale(LC_ALL,"spanish");
@@ -56,10 +47,10 @@ int main(){//Función principal
             case 4:consultas();break;
             case 5:despliegues();break;
             case 6:ordenes();break;
-            case 0:gotoxy(1,18);cout<<"FIN DEL PROGRAMA";break;
-            default:gotoxy(1,18);cout<<"Opción incorrecta";getch();break;
+            case 0:gotoxy(7,18);cout<<"FIN DEL PROGRAMA\n"<<endl;break;
+            default:gotoxy(7,18);cout<<"Opción incorrecta\n"<<endl;getch();break;
         }
-    }while(opcion>0);
+    }while(opcion!=0);
     return 0;
 }
 
@@ -88,31 +79,19 @@ void altas(){//Función para crear datos
         gotoxy(1,18);cout<<"La pila está llena."<<endl;cout<<"Intente otra opción."<<endl;
         getch();
     }else{
-        gotoxy(1,18);cout<<"Digite los datos: "<<endl;
-        fflush(stdin);
-        cout<<"Digite el nombre: ";cin.getline(empleados[marca].nombre,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el apellido paterno: ";cin.getline(empleados[marca].apellidopat,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el apellido materno: ";cin.getline(empleados[marca].apellidomat,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el email: ";cin.getline(empleados[marca].email,30,'\n');
-        fflush(stdin);
-        cout<<"Digita la edad: ";cin.getline(empleados[marca].edad,30,'\n');
-        fflush(stdin);
-        cout<<"Digite la calle donde vive: ";cin.getline(empleados[marca].dir_empleado.calle,30,'\n');
-        fflush(stdin);
-        cout<<"Digite la colonia donde vive: ";cin.getline(empleados[marca].dir_empleado.colonia,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el municipio donde vive: ";cin.getline(empleados[marca].dir_empleado.municipio,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el número exterior de su vivienda: ";cin.getline(empleados[marca].dir_empleado.numex,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el número interior de su vivienda: ";cin.getline(empleados[marca].dir_empleado.numin,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el código postal: ";cin.getline(empleados[marca].dir_empleado.cp,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el teléfono: ";cin.getline(empleados[marca].telefono,30,'\n');
+        gotoxy(1,18);cout<<"Digite los datos: "<<endl;fflush(stdin);
+        cout<<"Digite el nombre: ";cin>>e[marca].nombre;fflush(stdin);
+        cout<<"Digite el apellido paterno: ";cin>>e[marca].apellidopat;fflush(stdin);
+        cout<<"Digite el apellido materno: ";cin>>e[marca].apellidomat;fflush(stdin);
+        cout<<"Digite el email: ";cin>>e[marca].email;fflush(stdin);
+        cout<<"Digita la edad: ";cin>>e[marca].edad;fflush(stdin);
+        cout<<"Digite la calle donde vive: ";cin>>e[marca].dir.calle;fflush(stdin);
+        cout<<"Digite la colonia donde vive: ";cin>>e[marca].dir.colonia;fflush(stdin);
+        cout<<"Digite el municipio donde vive: ";cin>>e[marca].dir.municipio;fflush(stdin);
+        cout<<"Digite el número exterior de su vivienda: ";cin>>e[marca].dir.numex;fflush(stdin);
+        cout<<"Digite el número interior de su vivienda: ";cin>>e[marca].dir.numin;fflush(stdin);
+        cout<<"Digite el código postal: ";cin>>e[marca].dir.cp;fflush(stdin);
+        cout<<"Digite el teléfono: ";cin>>e[marca].telefono;
         marca++;
     }
 }
@@ -126,28 +105,28 @@ void bajas(){//Función para eliminar datos
         getch();
     }else{
         gotoxy(1,19);cout<<"Los datos de la posición "<<posicion<<" son: \n";
-        cout<<"Nombre: "<<empleados[posicion].nombre<<endl;
-        cout<<"Apellido paterno: "<<empleados[posicion].apellidopat<<endl;
-        cout<<"Apellido materno: "<<empleados[posicion].apellidomat<<endl;
-        cout<<"Email: "<<empleados[posicion].email<<endl;
-        cout<<"Edad: "<<empleados[posicion].edad<<endl;
-		cout<<"Calle: "<<empleados[posicion].dir_empleado.calle<<endl;
-		cout<<"Colonia: "<<empleados[posicion].dir_empleado.colonia<<endl;
-		cout<<"Municipio: "<<empleados[posicion].dir_empleado.municipio<<endl;
-		cout<<"Número exterior: "<<empleados[posicion].dir_empleado.numex<<endl;
-		cout<<"Número interior: "<<empleados[posicion].dir_empleado.numin<<endl;
-		cout<<"Código postal: "<<empleados[posicion].dir_empleado.cp<<endl;
-        cout<<"Teléfono: "<<empleados[posicion].telefono<<endl;
+        cout<<"Nombre: "<<e[posicion].nombre<<endl;
+        cout<<"Apellido paterno: "<<e[posicion].apellidopat<<endl;
+        cout<<"Apellido materno: "<<e[posicion].apellidomat<<endl;
+        cout<<"Email: "<<e[posicion].email<<endl;
+        cout<<"Edad: "<<e[posicion].edad<<endl;
+		cout<<"Calle: "<<e[posicion].dir.calle<<endl;
+		cout<<"Colonia: "<<e[posicion].dir.colonia<<endl;
+		cout<<"Municipio: "<<e[posicion].dir.municipio<<endl;
+		cout<<"Número exterior: "<<e[posicion].dir.numex<<endl;
+		cout<<"Número interior: "<<e[posicion].dir.numin<<endl;
+		cout<<"Código postal: "<<e[posicion].dir.cp<<endl;
+        cout<<"Teléfono: "<<e[posicion].telefono<<endl;
         cout<<"¿Desea dar de baja estos datos? s/n: ";cin>>decision;
         if(decision=='s'){
             if(posicion==99){
-                struct empleado empleados [posicion]={0};
+                struct persona e[posicion]={0};
                 marca-=1;
                 cout<<"Datos dados de baja.";
                 getch();
             }else{
                 for(int i=posicion;i<100;i++){
-                    empleados[i]=empleados[i+1];
+                    e[i]=e[i+1];
                 }
                 marca-=1;
                 cout<<"Datos dados de baja";
@@ -165,43 +144,32 @@ void cambios(){//Función para cambiar datos
         getch();
     }else{
         gotoxy(1,19);cout<<"Los datos de la posición "<<posicion<<" son: \n";
-        cout<<"Nombre: "<<empleados[posicion].nombre<<endl;
-        cout<<"Apellido paterno: "<<empleados[posicion].apellidopat<<endl;
-        cout<<"Apellido materno: "<<empleados[posicion].apellidomat<<endl;
-        cout<<"Email: "<<empleados[posicion].email<<endl;
-        cout<<"Edad: "<<empleados[posicion].edad<<endl;
-		cout<<"Calle: "<<empleados[posicion].dir_empleado.calle<<endl;
-		cout<<"Colonia: "<<empleados[posicion].dir_empleado.colonia<<endl;
-		cout<<"Municipio: "<<empleados[posicion].dir_empleado.municipio<<endl;
-		cout<<"Número exterior: "<<empleados[posicion].dir_empleado.numex<<endl;
-		cout<<"Número interior: "<<empleados[posicion].dir_empleado.numin<<endl;
-		cout<<"Código postal: "<<empleados[posicion].dir_empleado.cp<<endl;
-        cout<<"Teléfono: "<<empleados[posicion].telefono<<endl;                                            
-        cout<<"\n\nDigite los nuevos datos"<<endl;
-        fflush(stdin);
-        cout<<"Digite el nombre: ";cin.getline(empleados[posicion].nombre,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el apellido paterno: ";cin.getline(empleados[posicion].apellidopat,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el apellido materno: ";cin.getline(empleados[posicion].apellidomat,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el email: ";cin.getline(empleados[posicion].email,30,'\n');
-        fflush(stdin);
-        cout<<"Digite la edad: ";cin.getline(empleados[posicion].edad,30,'\n');
-        fflush(stdin);
-        cout<<"Digite la calle donde vive: ";cin.getline(empleados[posicion].dir_empleado.calle,30,'\n');
-        fflush(stdin);
-        cout<<"Digite la colonia donde vive: ";cin.getline(empleados[posicion].dir_empleado.colonia,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el municipio donde vive: ";cin.getline(empleados[posicion].dir_empleado.colonia,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el número exterior de su vivienda: ";cin.getline(empleados[posicion].dir_empleado.numex,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el número interior de su vivienda: ";cin.getline(empleados[posicion].dir_empleado.numin,30,'\n'); 
-        fflush(stdin);
-        cout<<"Digite el código postal: ";cin.getline(empleados[posicion].dir_empleado.cp,30,'\n');
-        fflush(stdin);
-        cout<<"Digite el teléfono: ";cin.getline(empleados[posicion].telefono,30,'\n');
+        cout<<"Nombre: "<<e[posicion].nombre<<endl;
+        cout<<"Apellido paterno: "<<e[posicion].apellidopat<<endl;
+        cout<<"Apellido materno: "<<e[posicion].apellidomat<<endl;
+        cout<<"Email: "<<e[posicion].email<<endl;
+        cout<<"Edad: "<<e[posicion].edad<<endl;
+		cout<<"Calle: "<<e[posicion].dir.calle<<endl;
+		cout<<"Colonia: "<<e[posicion].dir.colonia<<endl;
+		cout<<"Municipio: "<<e[posicion].dir.municipio<<endl;
+		cout<<"Número exterior: "<<e[posicion].dir.numex<<endl;
+		cout<<"Número interior: "<<e[posicion].dir.numin<<endl;
+		cout<<"Código postal: "<<e[posicion].dir.cp<<endl;
+        cout<<"Teléfono: "<<e[posicion].telefono<<endl;
+
+        cout<<"\n\nDigite los nuevos datos"<<endl;fflush(stdin);
+        cout<<"Digite el nombre: ";cin>>e[posicion].nombre;fflush(stdin);
+        cout<<"Digite el apellido paterno: ";cin>>e[posicion].apellidopat;fflush(stdin);
+        cout<<"Digite el apellido materno: ";cin>>e[posicion].apellidomat;fflush(stdin);
+        cout<<"Digite el email: ";cin>>e[posicion].email;fflush(stdin);
+        cout<<"Digite la edad: ";cin>>e[posicion].edad;fflush(stdin);
+        cout<<"Digite la calle donde vive: ";cin>>e[posicion].dir.calle;fflush(stdin);
+        cout<<"Digite la colonia donde vive: ";cin>>e[posicion].dir.colonia;fflush(stdin);
+        cout<<"Digite el municipio donde vive: ";cin>>e[posicion].dir.colonia;fflush(stdin);
+        cout<<"Digite el número exterior de su vivienda: ";cin>>e[posicion].dir.numex;fflush(stdin);
+        cout<<"Digite el número interior de su vivienda: ";cin>>e[posicion].dir.numin;fflush(stdin);
+        cout<<"Digite el código postal: ";cin>>e[posicion].dir.cp;fflush(stdin);
+        cout<<"Digite el teléfono: ";cin>>e[posicion].telefono;
         marca++;
         cout<<"Datos cambiados.";
         getch();
@@ -216,58 +184,71 @@ void consultas(){//Función para consultar una posición
         getch();
     }else{
         gotoxy(1,19);cout<<"Los datos de la posición "<<posicion<<" son: \n";
-        cout<<"Nombre: "<<empleados[posicion].nombre<<endl;
-        cout<<"Apellido paterno: "<<empleados[posicion].apellidopat<<endl;
-        cout<<"Apellido materno: "<<empleados[posicion].apellidomat<<endl;
-        cout<<"Email: "<<empleados[posicion].email<<endl;
-        cout<<"Edad: "<<empleados[posicion].edad<<endl;
-		cout<<"Calle: "<<empleados[posicion].dir_empleado.calle<<endl;
-		cout<<"Colonia: "<<empleados[posicion].dir_empleado.colonia<<endl;
-		cout<<"Municipio: "<<empleados[posicion].dir_empleado.municipio<<endl;
-		cout<<"Número exterior: "<<empleados[posicion].dir_empleado.numex<<endl;
-		cout<<"Número interior: "<<empleados[posicion].dir_empleado.numin<<endl;
-		cout<<"Código postal: "<<empleados[posicion].dir_empleado.cp<<endl;
-        cout<<"Teléfono: "<<empleados[posicion].telefono<<endl;
+        cout<<"Nombre: "<<e[posicion].nombre<<endl;
+        cout<<"Apellido paterno: "<<e[posicion].apellidopat<<endl;
+        cout<<"Apellido materno: "<<e[posicion].apellidomat<<endl;
+        cout<<"Email: "<<e[posicion].email<<endl;
+        cout<<"Edad: "<<e[posicion].edad<<endl;
+		cout<<"Calle: "<<e[posicion].dir.calle<<endl;
+		cout<<"Colonia: "<<e[posicion].dir.colonia<<endl;
+		cout<<"Municipio: "<<e[posicion].dir.municipio<<endl;
+		cout<<"Número exterior: "<<e[posicion].dir.numex<<endl;
+		cout<<"Número interior: "<<e[posicion].dir.numin<<endl;
+		cout<<"Código postal: "<<e[posicion].dir.cp<<endl;
+        cout<<"Teléfono: "<<e[posicion].telefono<<endl;
         getch();
     }
 }
 
-void despliegues(){//PREGUNTAR EL ORDEN DE DESPLIEGUE
+void despliegues(){//PREGUNTAR POR EL LÍMITE DE IMPRESIÓN
     gotoxy(1,18);cout<<"Datos de pila:\n";
     for(int i=0;i<99;i++){
-        cout<<"Nombre: "<<empleados[i].nombre<<endl;
-        cout<<"Apellido paterno: "<<empleados[i].apellidopat<<endl;
-        cout<<"Apellido materno: "<<empleados[i].apellidomat<<endl;
-        cout<<"Email: "<<empleados[i].email<<endl;
-        cout<<"Edad: "<<empleados[i].edad<<endl;
-		cout<<"Calle: "<<empleados[i].dir_empleado.calle<<endl;
-		cout<<"Colonia: "<<empleados[i].dir_empleado.colonia<<endl;
-		cout<<"Municipio: "<<empleados[i].dir_empleado.municipio<<endl;
-		cout<<"Número exterior: "<<empleados[i].dir_empleado.numex<<endl;
-		cout<<"Número interior: "<<empleados[i].dir_empleado.numin<<endl;
-		cout<<"Código postal: "<<empleados[i].dir_empleado.cp<<endl;
-        cout<<"Teléfono: "<<empleados[i].telefono<<endl;
-        cout<<"\n\n\n";
+        cout<<"Nombre: "<<e[i].nombre<<endl;
+        cout<<"Apellido paterno: "<<e[i].apellidopat<<endl;
+        cout<<"Apellido materno: "<<e[i].apellidomat<<endl;
+        cout<<"Email: "<<e[i].email<<endl;
+        cout<<"Edad: "<<e[i].edad<<endl;
+		cout<<"Calle: "<<e[i].dir.calle<<endl;
+		cout<<"Colonia: "<<e[i].dir.colonia<<endl;
+		cout<<"Municipio: "<<e[i].dir.municipio<<endl;
+		cout<<"Número exterior: "<<e[i].dir.numex<<endl;
+		cout<<"Número interior: "<<e[i].dir.numin<<endl;
+		cout<<"Código postal: "<<e[i].dir.cp<<endl;
+        cout<<"Teléfono: "<<e[i].telefono<<endl;
+        cout<<"\n";
 		fflush(stdin);
     } 
     getch();
 }
 
-void ordenes(struct ordenamiento a[]){
-    int i,j,contador=1;
-    struct ordenamiento temp;
+void ordenes(){//PREGUNTAR SI ESTÁ BIEN EL ORDEN Y EL LÍMITE
+    int i,j,contador=100;
+    persona tempo;
+    cout<<"\n\n";
     for(i=0;i<100;i++){
-        for(j=100;j>=i;j--){
-            if(a[j-1].apellido>a[j].apellido){
-                temp=a[j-1];
-                a[j-1]=a[j];
-                a[j]=temp;
+        for(j=0;j<99;j++){
+            if(e[j].apellidopat > e[j+1].apellidopat){
+                tempo=e[j];
+                e[j]=e[j+1];
+                e[j+1]=tempo;
             }
         }
     }
+
     for(i=0;i<100;i++){
-        cout<<contador<<".- "<<a[i].apellido<<endl;
-        contador+=1;
+        cout<<contador<<".- Nombre: "<<e[i].nombre<<endl;
+        cout<<"    Apellido paterno: "<<e[i].apellidopat<<endl;
+        cout<<"    Apellido materno: "<<e[i].apellidomat<<endl;
+        cout<<"    Email: "<<e[i].email<<endl;
+        cout<<"    Edad: "<<e[i].edad<<endl;
+        cout<<"    Calle: "<<e[i].dir.calle<<endl;
+        cout<<"    Colonia: "<<e[i].dir.colonia<<endl;
+        cout<<"    Municipio: "<<e[i].dir.municipio<<endl;
+        cout<<"    Numero exterior: "<<e[i].dir.numex<<endl;
+        cout<<"    Numero interior: "<<e[i].dir.numin<<endl;
+        cout<<"    Código postal: "<<e[i].dir.cp<<endl;
+        cout<<"    Teléfono: "<<e[i].telefono<<endl;
+        contador-=1;
     }
     getch();
 }
