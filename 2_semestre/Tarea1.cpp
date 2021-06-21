3211dc5ceae7aec0a3d8523da2ad7c74850514ff
marvaz10 <marvaz10@protonmail.com>
2021-06-21 13:30:31 -0500

    Fin de curso

--- 2_semestre/Tarea1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<locale.h>
-int main(){
-    setlocale(LC_ALL,"spanish");
-    printf("Nombre: Cristian Martín Vázquez González\nEdad:18 años\nTeléfono:5547353499.\nCorreo electrónico: marvaz10@protonmail.com");
-    return 0;
-}
--- 2_semestre/Tarea2.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main(){
-    int a=15;float b=10.045,c=16.3456;char d='A',e[]="Hola";
-    printf("%i\n%.2f\n%.4f\n%c\n%s", a,b,c,d,e);
-    return 0;
-}
