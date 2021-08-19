b95c79e562978955cd8a7c635496377833ee1640
marvaz10 <marvaz10@protonmail.com>
2021-08-19 11:23:35 -0500

    Tarea 04

---
+++ 3er_semestre/Tarea_04.c
@@ -0,0 +1,29 @@
+//Tarea 04 Indicar cual sería la salida de cada uno de los siguientes grupos de sentencias
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (){
+    //a)
+    printf("Historias de cronopios y famas. ");
+    printf("Autor: Julio Cortazar");
+    getch();
+    printf("\n\n");
+
+    //b)
+    printf("Cuantas lineas \nocupa esto?");
+    getch();
+    printf("\n\n");
+
+    //c)
+    printf("Estamos\naprendiendo /naprogramar en C");
+    getch();
+    printf("\n\n");
+
+    //d)
+    int num;
+    num=2;
+    printf("%d + %d = %d",num,num,num+num);
+    getch();
+    return 0;
+}
